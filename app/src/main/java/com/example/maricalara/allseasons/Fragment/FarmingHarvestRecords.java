73de5b975310c0adb894fbb415dff35f4e313da0
mariisy <renziecalara@gmail.com>
2018-05-11 18:07:17 +0800

    git comit mari: UI

--- app/src/main/java/com/example/maricalara/allseasons/Fragment/FarmingHarvestRecords.java
+++ app/src/main/java/com/example/maricalara/allseasons/Fragment/FarmingHarvestRecords.java
@@ -8,15 +8,29 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
 
+import com.example.maricalara.allseasons.Adapter.ExpandableListViewAdapter;
 import com.example.maricalara.allseasons.Adapter.ViewPagerAdapter;
+import com.example.maricalara.allseasons.Controller.PGAddData;
 import com.example.maricalara.allseasons.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class FarmingHarvestRecords extends Fragment {
 
     private TabLayout tabLayout;
     private ViewPager viewPager;
     private ViewPagerAdapter adapter;
+
+    //data
+    ExpandableListView expandableListView;
+    ExpandableListViewAdapter expandableListViewAdapter;
+    List<String> expandableListTitle;
+    HashMap<String, List<String>> expandableListDetail;
 
     public FarmingHarvestRecords() {
         // Required empty public constructor
@@ -32,6 +46,54 @@ public class FarmingHarvestRecords extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_farming_harvest_records, container, false);
+
+
+        //List
+        expandableListView = (ExpandableListView) rootView.findViewById(R.id.expandableListViewPGAdd);
+        expandableListDetail = PGAddData.getData();
+        expandableListTitle = new ArrayList<String>(expandableListDetail.keySet());
+        expandableListViewAdapter = new ExpandableListViewAdapter(getActivity(), expandableListTitle, expandableListDetail);
+
+
+        expandableListView.setAdapter(expandableListViewAdapter);
+
+        expandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                Toast.makeText(getActivity().getApplicationContext(),
+                        expandableListTitle.get(groupPosition) + " List Expanded.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        expandableListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                Toast.makeText(getActivity().getApplicationContext(),
+                        expandableListTitle.get(groupPosition) + " List Collapsed.",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                Toast.makeText(
+                        getActivity().getApplicationContext(),
+                        expandableListTitle.get(groupPosition)
+                                + " -> "
+                                + expandableListDetail.get(
+                                expandableListTitle.get(groupPosition)).get(
+                                childPosition), Toast.LENGTH_SHORT
+                ).show();
+                return false;
+            }
+        });
+
 
 
 
--- app/src/main/java/com/example/maricalara/allseasons/Fragment/FarmingUtilizedRecord.java
+++ app/src/main/java/com/example/maricalara/allseasons/Fragment/FarmingUtilizedRecord.java
@@ -8,15 +8,30 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
 
+import com.example.maricalara.allseasons.Adapter.ExpandableListViewAdapter;
 import com.example.maricalara.allseasons.Adapter.ViewPagerAdapter;
+import com.example.maricalara.allseasons.Controller.PGAddData;
 import com.example.maricalara.allseasons.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class FarmingUtilizedRecord extends Fragment {
 
     private TabLayout tabLayout;
     private ViewPager viewPager;
     private ViewPagerAdapter adapter;
+
+    //data
+    ExpandableListView expandableListView;
+    ExpandableListViewAdapter expandableListViewAdapter;
+    List<String> expandableListTitle;
+    HashMap<String, List<String>> expandableListDetail;
+
 
     public FarmingUtilizedRecord() {
         // Required empty public constructor
@@ -33,6 +48,48 @@ public class FarmingUtilizedRecord extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_farming_utilized_records, container, false);
 
+        //List
+        expandableListView = (ExpandableListView) rootView.findViewById(R.id.expandableListViewPGAdd);
+        expandableListDetail = PGAddData.getData();
+        expandableListTitle = new ArrayList<String>(expandableListDetail.keySet());
+        expandableListViewAdapter = new ExpandableListViewAdapter(getActivity(), expandableListTitle, expandableListDetail);
+
+
+        expandableListView.setAdapter(expandableListViewAdapter);
+
+        expandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                Toast.makeText(getActivity().getApplicationContext(),
+                        expandableListTitle.get(groupPosition) + " List Expanded.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        expandableListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                Toast.makeText(getActivity().getApplicationContext(),
+                        expandableListTitle.get(groupPosition) + " List Collapsed.",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                Toast.makeText(getActivity().getApplicationContext(),
+                        expandableListTitle.get(groupPosition) + " -> "
+                                + expandableListDetail.get(expandableListTitle.get(groupPosition)).get(
+                                childPosition), Toast.LENGTH_SHORT
+                ).show();
+                return false;
+            }
+        });
 
 
         // Inflate the layout for this fragment
