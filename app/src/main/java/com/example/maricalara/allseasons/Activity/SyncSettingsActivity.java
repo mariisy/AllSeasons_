60eaefaaa311a58ccb5efd01899b5ffbc35e2d54
mariisy <renziecalara@gmail.com>
2018-05-27 13:54:30 +0800

    - git commit mari: Wifi p2p

--- app/src/main/java/com/example/maricalara/allseasons/Activity/SyncSettingsActivity.java
+++ app/src/main/java/com/example/maricalara/allseasons/Activity/SyncSettingsActivity.java
@@ -24,20 +24,20 @@ import android.widget.Toast;
 import com.ajts.androidmads.library.SQLiteToExcel;
 import com.example.maricalara.allseasons.Model.DBHelper;
 import com.example.maricalara.allseasons.R;
-import com.example.maricalara.allseasons.WifiP2PReceiverClass.ServerService;
-import com.example.maricalara.allseasons.WifiP2PReceiverClass.WiFiServerBroadcastReceiver;
+import com.example.maricalara.allseasons.WifiP2P.Wifi.GlobalActivity;
+import com.example.maricalara.allseasons.WifiP2P.Wifi.WiFiDirectActivity;
 
 import java.io.File;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class SyncSettingsActivity extends AppCompatActivity implements WifiP2pManager.ChannelListener {
+public class SyncSettingsActivity extends AppCompatActivity  {
 
     private Toolbar toolbar;
 
     //UI
-    Button btnExport, btnStartSend, btnStartRecieve, btnBrowse;
+    Button btnExport, btnSync;
 
     //get Date String
     Date date = new Date();
@@ -50,23 +50,6 @@ public class SyncSettingsActivity extends AppCompatActivity implements WifiP2pMa
     //data variable
     String directory_path;
 
-    //Wifi P2P
-    public final int fileRequestID = 55;
-    public final int port = 7950;
-
-
-    private WifiP2pManager wifiManager;
-    private WifiP2pManager.Channel wifichannel;
-    private BroadcastReceiver wifiServerReceiver;
-
-    private IntentFilter wifiServerReceiverIntentFilter;
-
-    private String path;
-    private File downloadTargetFile;
-
-    private Intent serverServiceIntent;
-
-    private boolean serverThreadActive;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,65 +63,28 @@ public class SyncSettingsActivity extends AppCompatActivity implements WifiP2pMa
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        //Block auto opening keyboard
-        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
-
-        wifiManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
-        wifichannel = wifiManager.initialize(this, getMainLooper(), null);
-        wifiServerReceiver = new WiFiServerBroadcastReceiver(wifiManager, wifichannel, this);
-
-        wifiServerReceiverIntentFilter = new IntentFilter();
-        ;
-        wifiServerReceiverIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
-        wifiServerReceiverIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
-        wifiServerReceiverIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
-        wifiServerReceiverIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
-
-        //set status to stopped
-        TextView serverServiceStatus = (TextView) findViewById(R.id.server_status_text);
-        serverServiceStatus.setText("Server has stopped");
-
-        path = "/";
-        downloadTargetFile = new File(path);
-
-        serverServiceIntent = null;
-        serverThreadActive = false;
-
-        setServerFileTransferStatus("No File being transfered");
-
-        registerReceiver(wifiServerReceiver, wifiServerReceiverIntentFilter);
 
 
         //inflate UI
-
-
-
-        btnStartSend = (Button) findViewById(R.id.btnStartSend);
-        btnStartSend.setOnClickListener(new View.OnClickListener() {
+        btnExport = (Button) findViewById(R.id.btnExport);
+        btnExport.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                stopServer(null);
-                Intent clientStartIntent = new Intent(SyncSettingsActivity.this, SyncSettingsClient.class);
-                startActivity(clientStartIntent);
+
             }
         });
 
-        btnStartRecieve = (Button) findViewById(R.id.btnStartRecieve);
-        btnStartRecieve.setOnClickListener(new View.OnClickListener() {
+        btnSync = (Button) findViewById(R.id.btnSync);
+        btnSync.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                serverStart();
+                Intent intent = new Intent(SyncSettingsActivity.this, WiFiDirectActivity.class);
+                SyncSettingsActivity.this.startActivity(intent);
             }
         });
 
-        btnBrowse = (Button) findViewById(R.id.btnBrowse);
-        btnBrowse.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent clientStartIntent = new Intent(SyncSettingsActivity.this, SyncSettingsBrowse.class);
-                startActivityForResult(clientStartIntent, fileRequestID);
-            }
-        });
+
+
     }
 
     private void exportDB() {
@@ -178,186 +124,7 @@ public class SyncSettingsActivity extends AppCompatActivity implements WifiP2pMa
         }
     }
 
-    /*
-    *
-    *
-    all method that follows are used for the Wifi P2P transfer*
-    *
-    *
-    */
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        if (resultCode == Activity.RESULT_OK && requestCode == fileRequestID) {
-            //Fetch result
-            File targetDir = (File) data.getExtras().get("file");
-
-            if (targetDir.isDirectory()) {
-                if (targetDir.canWrite()) {
-                    downloadTargetFile = targetDir;
-                    TextView filePath = (TextView) findViewById(R.id.server_file_path);
-                    filePath.setText(targetDir.getPath());
-                    setServerFileTransferStatus("Download directory set to " + targetDir.getName());
-
-                } else {
-                    setServerFileTransferStatus("You do not have permission to write to " + targetDir.getName());
-                }
-
-            } else {
-                setServerFileTransferStatus("The selected file is not a directory. Please select a valid download directory.");
-            }
-
-        }
-    }
-
-    public void serverStart(){
-        //If server is already listening on port or transfering data, do not attempt to start server service
-        if (!serverThreadActive) {
-            //Create new thread, open socket, wait for connection, and transfer file
-
-            serverServiceIntent = new Intent(SyncSettingsActivity.this, ServerService.class);
-            serverServiceIntent.putExtra("saveLocation", downloadTargetFile);
-            serverServiceIntent.putExtra("port", new Integer(port));
-            serverServiceIntent.putExtra("serverResult", new ResultReceiver(null) {
-                @Override
-                protected void onReceiveResult(int resultCode, final Bundle resultData) {
-
-                    if (resultCode == port) {
-                        if (resultData == null) {
-                            //Server service has shut down. Download may or may not have completed properly.
-                            serverThreadActive = false;
 
 
-                            final TextView server_status_text = (TextView) findViewById(R.id.server_status_text);
-                            server_status_text.post(new Runnable() {
-                                public void run() {
-                                    server_status_text.setText("Server has stopped");
-                                }
-                            });
 
-
-                        } else {
-                            final TextView server_file_status_text = (TextView) findViewById(R.id.server_file_transfer_status);
-
-                            server_file_status_text.post(new Runnable() {
-                                public void run() {
-                                    server_file_status_text.setText((String) resultData.get("message"));
-                                }
-                            });
-                        }
-                    }
-
-                }
-            });
-
-            serverThreadActive = true;
-            startService(serverServiceIntent);
-
-            //Set status to running
-            TextView serverServiceStatus = (TextView) findViewById(R.id.server_status_text);
-            serverServiceStatus.setText("Server Running");
-
-        } else {
-            //Set status to already running
-            TextView serverServiceStatus = (TextView) findViewById(R.id.server_status_text);
-            serverServiceStatus.setText("The server is already running");
-
-        }
-    }
-
-    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-    public void stopServer(View view) {
-
-
-        //stop download thread
-        if (serverServiceIntent != null) {
-            //stopService(serverServiceIntent);
-            serverThreadActive = false;
-            wifiManager.clearServiceRequests(wifichannel, new WifiP2pManager.ActionListener() {
-                public void onSuccess() {
-                    TextView serverServiceStatus = (TextView) findViewById(R.id.server_status_text);
-                    serverServiceStatus.setText("The server has stopped");
-                }
-
-                public void onFailure(int i) {
-                    TextView serverServiceStatus = (TextView) findViewById(R.id.server_status_text);
-                    serverServiceStatus.setText("Disconnect failed. Reason : " + i);
-                    Log.d("wifidirect", "Disconnect failed. Reason :" + i);
-
-                }
-            });
-
-
-            /*
-            wifiManager.removeGroup(wifichannel, new ActionListener() {
-
-                public void onFailure(int reasonCode) {
-
-                     TextView serverServiceStatus = (TextView) findViewById(R.id.server_status_text);
-                    serverServiceStatus.setText("Disconnect failed. Reason : " + reasonCode);
-                    Log.d("wifidirect", "Disconnect failed. Reason :" + reasonCode);
-
-                }
-
-                public void onSuccess() {
-                    TextView serverServiceStatus = (TextView) findViewById(R.id.server_status_text);
-                    serverServiceStatus.setText("The server has stopped");
-                }
-            });*/
-
-        }
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        //stopServer(null);
-        //unregisterReceiver(wifiServerReceiver);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        stopServer(null);
-
-        //stopService(serverServiceIntent);
-
-        //Unregister broadcast receiver
-        try {
-            unregisterReceiver(wifiServerReceiver);
-        } catch (IllegalArgumentException e) {
-            // This will happen if the server was never running and the stop
-            // button was pressed.
-            // Do nothing in this case.
-        }
-    }
-
-    public void setServerWifiStatus(String message) {
-        TextView server_wifi_status_text = (TextView) findViewById(R.id.server_wifi_status_text);
-        server_wifi_status_text.setText(message);
-    }
-
-    public void setServerStatus(String message) {
-        TextView server_status_text = (TextView) findViewById(R.id.server_status_text_2);
-        server_status_text.setText(message);
-    }
-
-
-    public void setServerFileTransferStatus(String message) {
-        TextView server_status_text = (TextView) findViewById(R.id.server_file_transfer_status);
-        server_status_text.setText(message);
-    }
-
-    @Override
-    public void onChannelDisconnected() {
-
-    }
 }
--- app/src/main/java/com/example/maricalara/allseasons/Activity/SyncSettingsClient.java
+++ app/src/main/java/com/example/maricalara/allseasons/Activity/SyncSettingsClient.java
@@ -30,9 +30,6 @@ import android.widget.Toast;
 
 import com.example.maricalara.allseasons.Model.DBHelper;
 import com.example.maricalara.allseasons.R;
-import com.example.maricalara.allseasons.WifiP2PReceiverClass.ClientService;
-import com.example.maricalara.allseasons.WifiP2PReceiverClass.WiFiClientBroadcastReceiver;
-
 import java.io.File;
 import java.util.ArrayList;
 
@@ -44,25 +41,6 @@ public class SyncSettingsClient extends AppCompatActivity {
     //data variable
     String directory_path;
 
-    public final int fileRequestID = 98;
-    public final int port = 7950;
-
-
-    private WifiP2pManager wifiManager;
-    private WifiP2pManager.Channel wifichannel;
-    private BroadcastReceiver wifiClientReceiver;
-
-    private IntentFilter wifiClientReceiverIntentFilter;
-
-    private boolean connectedAndReadyToSendFile;
-
-    private boolean filePathProvided;
-    private File fileToSend;
-    private boolean transferActive;
-
-    private Intent clientServiceIntent;
-    private WifiP2pDevice targetDevice;
-    private WifiP2pInfo wifiInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,39 +48,12 @@ public class SyncSettingsClient extends AppCompatActivity {
         setContentView(R.layout.activity_sync_settings_client);
 
 
-        wifiManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
-
-        wifichannel = wifiManager.initialize(this, getMainLooper(), null);
-        wifiClientReceiver = new WiFiClientBroadcastReceiver(wifiManager, wifichannel, this);
-
-        wifiClientReceiverIntentFilter = new IntentFilter();
-        ;
-        wifiClientReceiverIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
-        wifiClientReceiverIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
-        wifiClientReceiverIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
-        wifiClientReceiverIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
-
-        connectedAndReadyToSendFile = false;
-        filePathProvided = false;
-        fileToSend = null;
-        transferActive = false;
-        clientServiceIntent = null;
-        targetDevice = null;
-        wifiInfo = null;
-
-        registerReceiver(wifiClientReceiver, wifiClientReceiverIntentFilter);
-
-        setClientFileTransferStatus("Client is currently idle");
-
-        //setTargetFileStatus("testing");
-
 
         btnBrowse = (Button) findViewById(R.id.btnBrowse);
         btnBrowse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent clientStartIntent = new Intent(SyncSettingsClient.this, SyncSettingsBrowse.class);
-                startActivityForResult(clientStartIntent, fileRequestID);
+
             }
         });
 
@@ -110,7 +61,7 @@ public class SyncSettingsClient extends AppCompatActivity {
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                searchForPeers();
+
             }
         });
 
@@ -118,15 +69,12 @@ public class SyncSettingsClient extends AppCompatActivity {
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sendFile();
+
             }
         });
     }
 
 
-    public void setTransferStatus(boolean status) {
-        connectedAndReadyToSendFile = status;
-    }
 
 
     @Override
@@ -140,249 +88,5 @@ public class SyncSettingsClient extends AppCompatActivity {
     }
 
 
-    public void setNetworkToReadyState(boolean status, WifiP2pInfo info, WifiP2pDevice device) {
-        wifiInfo = info;
-        targetDevice = device;
-        connectedAndReadyToSendFile = status;
-    }
-
-    private void stopClientReceiver() {
-        try {
-            unregisterReceiver(wifiClientReceiver);
-        } catch (IllegalArgumentException e) {
-            //This will happen if the server was never running and the stop button was pressed.
-            //Do nothing in this case.
-        }
-    }
-
-    public void searchForPeers() {
-
-        //Discover peers, no call back method given
-        // wifiManager.discoverPeers(wifichannel, null);
-
-        wifiManager.discoverPeers(wifichannel, new WifiP2pManager.ActionListener() {
-
-            public void onSuccess() {
-                Toast.makeText(SyncSettingsClient.this, "Discovery Initiated",
-                        Toast.LENGTH_SHORT).show();
-
-
-            }
-
-            public void onFailure(int reasonCode) {
-                Toast.makeText(SyncSettingsClient.this, "Discovery Failed. Reason: " + reasonCode,
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-
-    public void sendFile() {
-
-        //Only try to send file if there isn't already a transfer active
-        if (!transferActive) {
-            if (!filePathProvided) {
-                setClientFileTransferStatus("Select a file to send before pressing send");
-            } else if (!connectedAndReadyToSendFile) {
-                setClientFileTransferStatus("You must be connected to a server before attempting to send a file");
-            } else if (wifiInfo == null) {
-                setClientFileTransferStatus("Missing Wifi P2P information");
-            } else {
-                //Get fileExtension
-                String fileXtent = getFileExtension(fileToSend);
-
-                //Launch client service
-                clientServiceIntent = new Intent(this, ClientService.class);
-                clientServiceIntent.putExtra("fileXtent", fileXtent);
-                clientServiceIntent.putExtra("fileToSend", fileToSend);
-                clientServiceIntent.setType("*/*");
-                clientServiceIntent.putExtra("port", Integer.valueOf(port));
-
-                //clientServiceIntent.putExtra("targetDevice", targetDevice);
-                clientServiceIntent.putExtra("wifiInfo", wifiInfo);
-                clientServiceIntent.putExtra("clientResult", new ResultReceiver(null) {
-                    @Override
-                    protected void onReceiveResult(int resultCode, final Bundle resultData) {
-
-                        if (resultCode == port) {
-                            if (resultData == null) {
-                                //Client service has shut down, the transfer may or may not have been successful. Refer to message
-                                transferActive = false;
-                            } else {
-                                final TextView client_status_text = (TextView) findViewById(R.id.file_transfer_status);
-
-                                client_status_text.post(new Runnable() {
-                                    public void run() {
-                                        client_status_text.setText((String) resultData.get("message"));
-                                    }
-                                });
-                            }
-                        }
-
-                    }
-                });
-
-                transferActive = true;
-                startService(clientServiceIntent);
-                //end
-            }
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        //fileToSend
-        if (resultCode == Activity.RESULT_OK && requestCode == fileRequestID) {
-            //Fetch result
-
-           // File targetDir = new File(directory_path);
-            File targetDir = (File) data.getExtras().get("file");
-
-            if (targetDir.isFile()) {
-                if (targetDir.canRead()) {
-                    fileToSend = targetDir;
-                    filePathProvided = true;
-
-                    setTargetFileStatus(targetDir.getName() + " selected for file transfer");
-
-                } else {
-                    filePathProvided = false;
-                    setTargetFileStatus("You do not have permission to read the file " + targetDir.getName());
-                }
-
-            } else {
-                filePathProvided = false;
-                setTargetFileStatus("You may not transfer a directory, please select a single file");
-            }
-
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        //Continue to listen for wifi related system broadcasts even when paused
-        //stopClientReceiver();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        //Kill thread that is transferring data
-
-        //Unregister broadcast receiver
-        stopClientReceiver();
-    }
-
-    public void setClientWifiStatus(String message) {
-        TextView connectionStatusText = (TextView) findViewById(R.id.client_wifi_status_text);
-        connectionStatusText.setText(message);
-    }
-
-    public void setClientStatus(String message) {
-        TextView clientStatusText = (TextView) findViewById(R.id.client_status_text);
-        clientStatusText.setText(message);
-    }
-
-    public void setClientFileTransferStatus(String message) {
-        TextView fileTransferStatusText = (TextView) findViewById(R.id.file_transfer_status);
-        fileTransferStatusText.setText(message);
-    }
-
-    public void setTargetFileStatus(String message) {
-        TextView targetFileStatus = (TextView) findViewById(R.id.selected_filename);
-        targetFileStatus.setText(message);
-    }
-
-    public void displayPeers(final WifiP2pDeviceList peers) {
-//Dialog to show errors/status
-        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
-        dialog.setTitle("WiFi Direct File Transfer");
-
-        //Get list view
-        ListView peerView = (ListView) findViewById(R.id.peers_listview);
-
-        //Make array list
-        ArrayList<String> peersStringArrayList = new ArrayList<String>();
-
-        //Fill array list with strings of peer names
-        for (WifiP2pDevice wd : peers.getDeviceList()) {
-            peersStringArrayList.add(wd.deviceName);
-        }
-
-        //Set list view as clickable
-        peerView.setClickable(true);
-
-        //Make adapter to connect peer data to list view
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, peersStringArrayList.toArray());
-
-        //Show peer data in listview
-        peerView.setAdapter(arrayAdapter);
-
-
-        peerView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            public void onItemClick(AdapterView<?> arg0, View view, int arg2, long arg3) {
-                //Get string from textview
-                TextView tv = (TextView) view;
-
-                WifiP2pDevice device = null;
-                //Search all known peers for matching name
-                for (WifiP2pDevice wd : peers.getDeviceList()) {
-                    if (wd.deviceName.equals(tv.getText()))
-                        device = wd;
-                }
-
-                if (device != null) {
-                    //Connect to selected peer
-                    connectToPeer(device);
-                } else {
-                    dialog.setMessage("Failed");
-                    dialog.show();
-                }
-            }
-            // TODO Auto-generated method stub
-        });
-    }
-
-    public void connectToPeer(final WifiP2pDevice wifiPeer) {
-        this.targetDevice = wifiPeer;
-
-        WifiP2pConfig config = new WifiP2pConfig();
-        config.deviceAddress = wifiPeer.deviceAddress;
-        wifiManager.connect(wifichannel, config, new WifiP2pManager.ActionListener() {
-            public void onSuccess() {
-                Toast.makeText(SyncSettingsClient.this, "Connection to " + targetDevice.deviceName + " Successful",
-                        Toast.LENGTH_SHORT).show();
-            }
-
-            public void onFailure(int reason) {
-
-                Toast.makeText(SyncSettingsClient.this, "Connection to " + targetDevice.deviceName + " Failed",
-                        Toast.LENGTH_SHORT).show();
-                //setClientStatus("Connection to " + targetDevice.deviceName + " failed");
-
-            }
-        });
-
-    }
-
-    public String getFileExtension(File file) {
-        Uri fileUri = Uri.fromFile(file);
-        String extension = null;
-        try {
-            extension = MimeTypeMap.getFileExtensionFromUrl(fileUri.toString());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return extension;
-    }
 
 }
